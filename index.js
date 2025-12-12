1dd05c1c9c49565272c3ce11009b04b66e0c5491
Suvro Biswas <shuvo.249963@gmail.com>
2025-12-13 01:47:38 +0600

    Install firebase admin and set the require for index.js

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const admin = require('firebase-admin')
 const port = 3000
+
 
 app.use(cors())
 app.use(express.json())
